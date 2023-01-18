29cdaccaf208faaabde2637fdf7fd616efe4eea7
Jorge Del Barco <jdelbarcogarza@gmail.com>
2023-01-18 15:55:51 -0600

    se agrego link a login view

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,9 +1,26 @@
-<script setup lang="ts">
-import LoginForm from '../components/loginForm/LoginForm.vue'
-</script>
 
 <template>
   <main>
-    <LoginForm />
+    <h4 class="text-center">Registrate o inicia sesion en la plataforma</h4>
+    <div class="row justify-center">
+      <SignUpForm />
+    </div>
+
+    <p class="q-pt-sm text-blue-grey-4 text-center">Already have an account? <RouterLink :to="{name: 'login'}" >Login</RouterLink></p>
+
+    <q-btn @click="btnHandler" label="redirect me" color="secondary" />
   </main>
 </template>
+
+<script setup lang="ts">
+import SignUpForm from '../components/loginForm/SignUpForm.vue'
+import router from '@/router';
+
+const btnHandler = () => {
+
+  console.log('redirecciono')
+  router.push({ name: 'home' })
+
+}
+
+</script>
