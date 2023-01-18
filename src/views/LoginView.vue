01bc2fe352182e05a6f3b2d0fab0276a233f6344
Jorge Del Barco <jdelbarcogarza@gmail.com>
2023-01-18 16:26:21 -0600

    funciona componente

---
+++ src/views/LoginView.vue
@@ -0,0 +1,22 @@
+
+<template>
+  <main>
+    <h4 class="text-center">Registrate o inicia sesion en la plataforma</h4>
+
+    <div class="row justify-center ">
+      <LoginForm />
+    </div>
+
+    <p class="q-pt-sm text-blue-grey-4 text-center">Already have an account? <RouterLink :to="{name: 'home'}" >Sign up</RouterLink></p>
+
+  </main>
+</template>
+
+<script lang="ts" setup>
+
+import SignUpForm from '../components/loginForm/SignUpForm.vue'
+import LoginForm from '../components/loginForm/LoginForm.vue'
+
+import router from '@/router';
+
+</script>
