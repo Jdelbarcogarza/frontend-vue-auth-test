c82259b97e33aab557cf3799c4f8d616ed74c281
Jorge Del Barco <jdelbarcogarza@gmail.com>
2023-01-18 16:53:51 -0600

    paginas de testing de vue router sin auth

--- src/components/AppBar.vue
+++ src/components/AppBar.vue
@@ -8,6 +8,7 @@
           </q-avatar>
           Title
         </q-toolbar-title>
+        <p><router-link to="about">About</router-link> | <router-link to="/">Home</router-link></p>
       </q-toolbar>
     </q-header>
 </template>
