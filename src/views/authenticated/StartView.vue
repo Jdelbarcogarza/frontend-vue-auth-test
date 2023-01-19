1c3b456f9db821427f637d8917c4ce80fbb98abc
Jorge Del Barco <jdelbarcogarza@gmail.com>
2023-01-19 16:29:18 -0600

    se usa store para abrir y cerrar drawer. NO FUNCIONA

--- src/views/authenticated/StartView.vue
+++ src/views/authenticated/StartView.vue
@@ -1,9 +1,32 @@
 <template>
 
   <h2>START (authenticated)</h2>
+  <p>Token {{ authToken }}</p>
+
+  <q-drawer show-if-above v-model="isDrawerOpen" side="left" bordered>
+      <!-- drawer content -->
+  </q-drawer>
 
 </template>
 
 <script lang="ts" setup>
+
+import { useAuthTokenStore } from '@/stores/authTokenStore';
+import { useToggleDrawer } from '@/stores/uiStores/drawerStore';
+
+import { ref } from 'vue'
+
+const authStore = useAuthTokenStore()
+const openDrawerStore = useToggleDrawer()
+
+const authToken = authStore.getAuthToken()
+const isDrawerOpen = openDrawerStore.isDrawerOpen
+
+const authTokenRef = ref<string>('')
+
+authTokenRef.value = authToken
+
+
+
 
 </script>
