47fc6144521823cb435fa2dfecbc298298f0f5de
Jorge Del Barco <jdelbarcogarza@gmail.com>
2023-01-19 16:28:40 -0600

    se agrego verificacion de auth en el frontend

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,4 @@
-<script setup lang="ts">
-import { RouterLink, RouterView } from 'vue-router'
-import HelloWorld from './components/HelloWorld.vue'
-import AppBar from './components/AppBar.vue';
 
-</script>
 
 <template>
   
@@ -17,6 +12,22 @@ import AppBar from './components/AppBar.vue';
     <q-page-container>
       <RouterView />
     </q-page-container>
+
   </q-layout>
 
 </template>
+
+<script setup lang="ts">
+import { RouterLink, RouterView } from 'vue-router'
+import HelloWorld from './components/HelloWorld.vue'
+import AppBar from './components/AppBar.vue';
+
+import { ref } from 'vue'
+import { isUserAthenticated } from './utils/isUserAuthenticated'
+
+const userIsAuthenticated = ref<boolean>(false);
+
+userIsAuthenticated.value = isUserAthenticated()
+
+
+</script>
