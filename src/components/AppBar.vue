6495a7dab9c5abe84ac0160314b3b6334d805001
Jorge Del Barco <jdelbarcogarza@gmail.com>
2023-01-19 16:29:02 -0600

    NO FUNCIONA interactividad con drawe

--- src/components/AppBar.vue
+++ src/components/AppBar.vue
@@ -2,6 +2,7 @@
 
     <q-header bordered class="bg-primary text-white">
       <q-toolbar>
+        <q-btn dense flat round icon="menu" @click="toggleDrawer" />
         <q-toolbar-title>
           <q-avatar>
             <img src="https://cdn.quasar.dev/logo-v2/svg/logo-mono-white.svg">
@@ -14,5 +15,14 @@
 </template>
 
 <script lang="ts" setup>
+
+import { isUserAthenticated } from '@/utils/isUserAuthenticated';
+import { useToggleDrawer } from '@/stores/uiStores/drawerStore';
+
+const store = useToggleDrawer()
+
+const toggleDrawer = () => {
+  store.toggleDrawer()
+}
 
 </script>
