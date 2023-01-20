e3875f0241d3ca50a6aacc2652f4eaf1f99da24a
Jorge Del Barco <jdelbarcogarza@gmail.com>
2023-01-20 12:11:18 -0600

    se agrego ruta al drawer

--- src/views/authenticated/StartView.vue
+++ src/views/authenticated/StartView.vue
@@ -1,34 +1,64 @@
 <template>
+	<h2>START (authenticated)</h2>
+	<p>Token {{ authToken }}</p>
 
-  <h2>START (authenticated)</h2>
-  <p>Token {{ authToken }}</p>
+	<q-drawer show-if-above v-model="isDrawerOpen" side="left" bordered>
+		<q-item>
+			<q-item-section>
+				<q-avatar>
+					<img src="src\assets\images\test image.jpg" alt="avatar" />
+				</q-avatar>
+			</q-item-section>
 
-  <q-drawer show-if-above v-model="isDrawerOpen" side="left" bordered>
-      <!-- drawer content -->
-  </q-drawer>
+			<q-item-section>
+				<p>jeb@email.com</p>
+				<p>$99.34</p>
+			</q-item-section>
+		</q-item>
+		<!-- drawer content -->
 
+		<q-separator />
+
+		<!-- encerrar esto en un template -->
+		<q-item clickable v-ripple>
+			<q-item-section avatar>
+				<p class="bg-purple-2">ICON</p>
+			</q-item-section>
+			<q-item-section>
+				<RouterLink :to="{ name: 'start' }">Agregar prospecto</RouterLink>
+			</q-item-section>
+		</q-item>
+		<q-separator />
+
+		<div class="column q-pt-xl">
+			<div class="q-py-sm">item 1</div>
+
+			<div class="q-py-sm">item 2</div>
+
+			<div class="q-py-sm">item 3</div>
+		</div>
+	</q-drawer>
 </template>
 
 <script lang="ts" setup>
+import { useAuthTokenStore } from "@/stores/authTokenStore";
+import { useToggleDrawer } from "@/stores/uiStores/drawerStore";
 
-import { useAuthTokenStore } from '@/stores/authTokenStore';
-import { useToggleDrawer } from '@/stores/uiStores/drawerStore';
+import { ref } from "vue";
+import { storeToRefs } from "pinia";
 
-import { ref } from 'vue'
-import { storeToRefs } from 'pinia';
+// avatar test image
+import avatarTestImage from "../../assets/images/test image.jpg";
+import { RouterLink } from "vue-router";
 
-const authStore = useAuthTokenStore()
-const openDrawerStore = useToggleDrawer()
+const authStore = useAuthTokenStore();
+const openDrawerStore = useToggleDrawer();
 
-const authToken = authStore.getAuthToken()
+const authToken = authStore.getAuthToken();
 
-const { isDrawerOpen } = storeToRefs(openDrawerStore)
+const { isDrawerOpen } = storeToRefs(openDrawerStore);
 
-const authTokenRef = ref<string>('')
+const authTokenRef = ref<string>("");
 
-authTokenRef.value = authToken
-
-
-
-
+authTokenRef.value = authToken;
 </script>
